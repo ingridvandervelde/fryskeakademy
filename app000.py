7bdfa6c9e1b4c31adc25379d6a5d71b9c924a8b6
WH <wheeringa@fryske-akademy.nl>
2022-12-12 18:41:04 +0100

    Add files via upload

---
+++ app000.py
@@ -0,0 +1,83 @@
+# pip install scikit-learn i.p.v. sklearn
+# shiny run --reload ~/Data/onderwijs/DataWise/_code/app.py
+
+from shiny import *
+import numpy as np
+
+from scipy.cluster import hierarchy
+import matplotlib.pyplot as plt
+
+import pandas as pd
+from sklearn.manifold import MDS
+
+choices = {
+  "Afrikaans": "Afrikaans", "Danish": "Danish", "Dutch": "Dutch", 
+  "English": "English", "Faroese": "Faroese", "Frisian": "Frisian", 
+  "German": "German", 
+  "Icelandic": "Icelandic", "Gronings": "Gronings"
+}
+
+app_ui = ui.page_fluid(
+  ui.input_select("x1", "Choose language 1", choices),
+  ui.input_select("x2", "Choose language 2", choices),
+  ui.output_text_verbatim("txt"),
+  ui.output_plot("dendrogram"),
+  ui.output_plot("multidimensional_clustering")
+)
+
+def server(input, output, session):
+  @output
+  @render.text
+  def txt():
+    return f"You chose languages {input.x1()} and {input.x2()}"
+  
+  @output
+  @render.plot
+  def dendrogram():
+    X = np.random.rand(15, 12)
+    fig = plt.figure()
+
+  # see: https://shiny.rstudio.com/py/docs/ui-page-layouts.html
+  # and  https://www.askpython.com/python/examples/dendrograms-in-python
+    dn = hierarchy.dendrogram(hierarchy.linkage(X, 'ward'), 
+                              above_threshold_color="blue", 
+                              color_threshold=0,
+                              orientation='right') # or: 'average'
+    return(fig)
+
+
+  @output
+  @render.plot
+  def multidimensional_clustering():
+    
+  # see: https://www.statology.org/multidimensional-scaling-in-python/
+    
+  # create DataFrane
+    df = pd.DataFrame({'player': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'],
+                       'points': [4, 4, 6, 7, 8, 14, 16, 19, 25, 25, 28],
+                       'assists': [3, 2, 2, 5, 4, 8, 7, 6, 8, 10, 11],
+                       'blocks': [7, 3, 6, 7, 5, 8, 8, 4, 2, 2, 1],
+                       'rebounds': [4, 5, 5, 6, 5, 8, 10, 4, 3, 2, 2]})
+
+  # set player column as index column
+    df = df.set_index('player')
+
+  # perform multi-dimensional scaling
+    mds = MDS(random_state=0)
+    scaled_df = mds.fit_transform(df)
+    
+  # create scatterplot
+    fig = plt.scatter(scaled_df[:,0], scaled_df[:,1])
+
+  # add axis labels
+    plt.xlabel('Coordinate 1')
+    plt.ylabel('Coordinate 2')
+
+  # add lables to each point
+    for i, txt in enumerate(df.index):
+      plt.annotate(txt, (scaled_df[:,0][i]+.3, scaled_df[:,1][i]))
+
+  # display scatterplot
+    return(fig)
+
+app = App(app_ui, server)
