5dc5f4dcf6385e78f4955130a844992cbd3fe495
MariaEsanu <m.esanu@student.rug.nl>
2022-12-14 12:50:28 +0100

    init mds and cluster analysis

--- cluster_analysis_maria.py
+++ cluster_analysis_maria.py
@@ -1,4 +1,5 @@
-from distance_calculations import *
+from distance_calculations import conllu_to_counter, calculate_distance_pairwise, calculate_distance_all
+from mds_cluster import create_dendogram
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -17,12 +18,12 @@ lang = {
 
 df = calculate_distance_all(lang)
 
+
+
 df.info()
 
 # do mds with df
 
+create_dendogram(df)
 
-def mds(distances: pd.DataFrame):
-    # do calculations
-    dendogram = 'filled dendogram'
-    return dendogram
+
