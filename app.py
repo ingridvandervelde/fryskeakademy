b315c34185ebc69caebfd7263177f399ca4b3f80
athalia <timmania@solcon.nl>
2023-01-10 15:20:59 +0100

    implement joep's part fixed misc stuff

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from shiny import *
 from shiny.types import FileInfo
 
 import numpy as np
-import pandas as pd
 
 from scipy.cluster import hierarchy
 import matplotlib.pyplot as plt
@@ -16,15 +15,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.manifold import MDS
 
-from distance_calculations import conllu_to_counter, calculate_distance_all
+from distance_calculations import conllu_to_counter, calculate_distance_all, calculate_distance_pairwise
 from collections import Counter
 
 from shiny.types import ImgData
 
-from mds_cluster import create_dendogram
+from mds_cluster import create_dendogram, create_mds
 
-
-### User Interace 
+### User Interace
 
 choices = {
     "Afrikaans": "Afrikaans", "Danish": "Danish", "Dutch": "Dutch",
@@ -33,56 +31,56 @@ choices = {
     "Icelandic": "Icelandic", "Gronings": "Gronings"
 }
 
-
 app_ui = ui.page_fluid(
     # https://shiny.rstudio.com/py/api/reference/shiny.ui.input_file.html
     ui.navset_tab_card(
-        ui.nav("Homepage", 
-                ui.panel_title("Syntactic Distance Calculator"),
-                  
-                ui.panel_main(
-                ui.output_text("txt"),
-                ui.output_image("image1"),
-                ),
-              ),
-        ui.nav("Upload CoNLL-U files",
-                ui.panel_title("Use your own CoNLL-U files"),
-                
-                ui.layout_sidebar(
-                  
-                  ui.panel_sidebar(
-                  ui.input_file("files", "Upload CoNLL-U files", accept=[".conllu"], multiple=True),
-                  #ui.input_text("x1", "Type language corresponding to the CoNLL-U file", placeholder="Enter language"),
-                  ui.output_ui("text_box"),
-                  ui.input_action_button("x1", "Calculate"),
-                  ui.output_table("show_text"),
-                  ),
-                  
-                  ui.panel_main(
-                  ui.output_plot("dendrogram1"),
-                  ui.output_plot("multidimensional_clustering1"),
-                  ),
-                ),
-              ),
-        ui.nav("List of languages",
-                ui.panel_title("Choose from a list of languages"),
-                
-                ui.layout_sidebar(
-                  
-                  ui.panel_sidebar(
-                  ui.input_checkbox_group("x2", "Select multiple languages", choices),
-                  ui.input_action_button("x2", "Calculate", class_="btn-success"),
-                  ),
-                  
-                  ui.panel_main(
-                  ui.output_plot("dendrogram2"),
-                  ui.output_plot("multidimensional_clustering2")
-                  ),
+        ui.nav("Homepage",
+               ui.panel_title("Syntactic Distance Calculator"),
+
+               ui.panel_main(
+                   ui.output_text("txt"),
+                   ui.output_image("image1"),
                ),
-            ),
-    # ui.output_plot("dendrogram"),
-    # ui.output_plot("multidimensional_clustering")
-  ), 
+               ),
+        ui.nav("Upload CoNLL-U files",
+               ui.panel_title("Use your own CoNLL-U files"),
+
+               ui.layout_sidebar(
+
+                   ui.panel_sidebar(
+                       ui.input_file("files", "Upload CoNLL-U files", accept=[".conllu"], multiple=True),
+                       # ui.input_text("x1", "Type language corresponding to the CoNLL-U file", placeholder="Enter language"),
+                       ui.output_ui("text_box"),
+                       ui.input_switch("pairwise", "Pairwise comparison"),
+                       ui.input_action_button("x1", "Calculate"),
+                   ),
+
+                   ui.panel_main(
+                       ui.output_table("show_text"),
+                       ui.output_plot("dendrogram1"),
+                       ui.output_plot("multidimensional_clustering1"),
+                   ),
+               ),
+               ),
+        # ui.nav("List of languages",
+        #         ui.panel_title("Choose from a list of languages"),
+        #
+        #         ui.layout_sidebar(
+        #
+        #           ui.panel_sidebar(
+        #           ui.input_checkbox_group("x2", "Select multiple languages", choices),
+        #           ui.input_action_button("x2", "Calculate", class_="btn-success"),
+        #           ),
+        #
+        #           ui.panel_main(
+        #           ui.output_plot("dendrogram2"),
+        #           ui.output_plot("multidimensional_clustering2")
+        #           ),
+        #        ),
+        #     ),
+        # ui.output_plot("dendrogram"),
+        # ui.output_plot("multidimensional_clustering")
+    ),
 )
 
 
@@ -93,6 +91,7 @@ def server(input, output, session):
     languages_counters = reactive.Value(dict())
     filenames = reactive.Value([])
     lang_distances = reactive.Value(pd.DataFrame())
+
     # create data structure containing conllu files
 
     @reactive.Effect
@@ -100,16 +99,19 @@ def server(input, output, session):
     def allfiles():
         local_dict = dict()
         files: list[FileInfo] = input.files()
+        # show progress bar
+        with ui.Progress(min=0, max=len(files)+1) as p:
+            p.set(message="Calculation in progress", detail="This may take a while...")
 
-        for file in files:
-            path = file['datapath']
-            filename = file['name']
-
-            local_dict[filename] = conllu_to_counter(path)
+            # files to counter objects
+            for i, file in enumerate(files):
+                path = file['datapath']
+                filename = file['name']
+                p.set(i+1, message=f"Computing {filename}")
+                local_dict[filename] = conllu_to_counter(path)
 
         dict_languages.set(local_dict)
 
-        
     @output
     @render.ui
     @reactive.event(input.files)
@@ -118,8 +120,9 @@ def server(input, output, session):
 
         filenames.set([f['name'] for f in files])
 
-        return ui.input_text("n", f"Type language(s) corresponding to the CoNLL-U file(s) separated by a ', '. Example: 'Dutch, English, Frisian'\nOrder: {','.join(filenames.get())}", placeholder="Enter language(s)")
-
+        return ui.input_text("n",
+                             f"Type language(s) corresponding to the CoNLL-U file(s) separated by a ', '. Example: 'Dutch, English, Frisian'\nOrder: {','.join(filenames.get())}",
+                             placeholder="Enter language(s)")
 
     @output
     @render.table
@@ -132,10 +135,18 @@ def server(input, output, session):
 
         languages_counters.set({file_lang[k]: v for k, v in dict_languages.get().items()})
 
-        distances = calculate_distance_all(languages_counters.get())
-        lang_distances.set(distances)
-        return lang_distances.get()
+        distances: pd.DataFrame
+        if input.pairwise():
+            distances = calculate_distance_pairwise(languages_counters.get())
+        else:
+            distances = calculate_distance_all(languages_counters.get())
 
+        lang_distances.set(distances)
+
+        # copy df to add languages in first column
+        output_table = distances.copy()
+        output_table.insert(0, "Languages", distances.index)
+        return output_table
 
     # def ask_language(dict_languages: dict[str, Counter]):
     #     languages_counters: dict[str: Counter]
@@ -161,40 +172,22 @@ def server(input, output, session):
     # 
     #     return languages_counters
 
-
     # respond on 'allfiles', and calculate distances between conllu files
 
     # respond on 'distance matrix' and show dendrogram and multidimensional scaling plot
 
-
-  
-    
     @output
     @render.plot
     @reactive.event(input.x1)
-  
     def dendrogram1():
-      fig = plt.figure()
-      dnd = create_dendogram(lang_distances.get())
-      
-    
-      return fig
-      
-        # X = np.random.rand(15, 12)
-        # fig = plt.figure()
-        # 
-        # # see: https://shiny.rstudio.com/py/docs/ui-page-layouts.html
-        # # and  https://www.askpython.com/python/examples/dendrograms-in-python
-        # dn = hierarchy.dendrogram(hierarchy.linkage(X, 'ward'),
-        #                           above_threshold_color="blue",
-        #                           color_threshold=0,
-        #                           orientation='right')  # or: 'average'
-        # return (fig)
-      
+        fig, ax = plt.subplots()
+        dnd = create_dendogram(lang_distances.get(), ax)
+
+        return fig
+
     @output
     @render.plot
     @reactive.event(input.x2)
-  
     def dendrogram2():
         X = np.random.rand(15, 12)
         fig = plt.figure()
@@ -206,50 +199,19 @@ def server(input, output, session):
                                   color_threshold=0,
                                   orientation='right')  # or: 'average'
         return (fig)
-    
-    
+
     @output
     @render.plot
     @reactive.event(input.x1)
-   
     def multidimensional_clustering1():
+        fig, ax = plt.subplots()
+        mds = create_mds(lang_distances.get(), ax)
 
-        # see: https://www.statology.org/multidimensional-scaling-in-python/
+        return fig
 
-        # create DataFrane
-        df = pd.DataFrame({'player': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'],
-                           'points': [4, 4, 6, 7, 8, 14, 16, 19, 25, 25, 28],
-                           'assists': [3, 2, 2, 5, 4, 8, 7, 6, 8, 10, 11],
-                           'blocks': [7, 3, 6, 7, 5, 8, 8, 4, 2, 2, 1],
-                           'rebounds': [4, 5, 5, 6, 5, 8, 10, 4, 3, 2, 2]})
-
-        # set player column as index column
-        df = df.set_index('player')
-
-        # perform multi-dimensional scaling
-        mds = MDS(random_state=0)
-        scaled_df = mds.fit_transform(df)
-
-        # create scatterplot
-        fig = plt.scatter(scaled_df[:, 0], scaled_df[:, 1])
-
-        # add axis labels
-        plt.xlabel('Coordinate 1')
-        plt.ylabel('Coordinate 2')
-
-        # add lables to each point
-        for i, txt in enumerate(df.index):
-            plt.annotate(txt, (scaled_df[:, 0][i] + .3, scaled_df[:, 1][i]))
-
-        # display scatterplot
-        return (fig)
-      
-      
-      
     @output
     @render.plot
     @reactive.event(input.x2)
-   
     def multidimensional_clustering2():
 
         # see: https://www.statology.org/multidimensional-scaling-in-python/
@@ -281,20 +243,20 @@ def server(input, output, session):
 
         # display scatterplot
         return (fig)
-    
+
     @output
     @render.image
-    def image1(): 
+    def image1():
         from pathlib import Path
         dir = Path(__file__).resolve().parent
         img: ImgData = {"src": str(dir / "data/Fryke_akademy.jpeg.jpeg"), "width": "300px"}
         return img
-        
+
     @output
     @render.text
     def txt():
         return f" Welcome to the website blablabla"
-      
+
     # @output
     # @render.image
     # def image2(): 
@@ -302,6 +264,7 @@ def server(input, output, session):
     #     dir = Path(__file__).resolve().parent
     #     img: ImgData = {"src": str(dir / """), "width": "150px"}
     #     return img
+
 
 app = App(app_ui, server)
 
--- mds_cluster/mds.py
+++ mds_cluster/mds.py
@@ -1,72 +1,89 @@
-from conll_u_reader import conllu_to_counter
-from distance_calculations import calculate_distance_all , calculate_distance_pairwise
 from sklearn.manifold import MDS
 import pandas as pd
 import matplotlib.pyplot as plt
 
-files = {
-    'Afrikaans': 'data/af_afribooms-ud-train.conllu',
-    'Danish': 'data/da_ddt-ud-train.conllu',
-    'German': 'data/de_gsd-ud-train.conllu',
-    'English': 'data/en_ewt-ud-train.conllu',
-    'Faroese': 'data/fo_oft-ud-test.conllu',
-    'Frisian': 'data/fy-frysk-ud-all.conllu',
-    'Icelandic': 'data/is_modern-ud-train.conllu',
-    'Gronings': 'data/nds-gronings-ud-all.conllu',
-    'Dutch': 'data/nl_alpino-ud-train.conllu',
-    'Norwegian': 'data/no_bokmaal-ud-train.conllu',
-    'Swedish': 'data/sv_talbanken-ud-train.conllu'
-}
 
-lang = dict()
+def create_mds(distances: pd.DataFrame, ax: plt.Axes):
+    mds = MDS(random_state=0)
+    scaled_df = mds.fit_transform(distances)
 
-for language, file in files.items():
-    lang[language] = conllu_to_counter(file)
-df = calculate_distance_all(lang)
-dfp = calculate_distance_pairwise(lang)
+    # create scatterplot
+    ax.scatter(scaled_df[:, 0], scaled_df[:, 1])
 
+    # add axis labels
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
 
-#perform multi-dimensional scaling
-mds = MDS(random_state=0)
-scaled_df = mds.fit_transform(df)
+    # add lables to each point
+    for i, txt in enumerate(distances.index):
+        ax.annotate(txt, (scaled_df[:, 0][i], scaled_df[:, 1][i]))
 
-#calculations for explained variance
-list1 = [0.49640871, 0.66261855, 0.7659065, 0.83728937, 0.88780632, 0.93446863,
- 0.95885534, 0.97855612, 0.99303231, 1, 1] #This is from the pca analysis not sure if this is the one to use though
+    return scaled_df
 
-def distance(x1,y1,x2,y2):
-    dist = ((x1-x2)**2 + (y1-y2)**2)**.5
-    return(dist)
-list2 = []
-for i in range(13):
-    if i == 10:
-        break
-    list2.append(distance(scaled_df[i,0], scaled_df[i,1], scaled_df[i+1,0], scaled_df[i+1,1]))
-
-
-def variance(a,b):
-    var = (a - b)**2
-    return(var)
-list3 = []
-for i in range(11):
-    if i == 10:
-        break
-    list3.append(variance(list1[i],list2[i]))
-
-explained_variance = str(sum(list3))
-
-
-
-#create scatterplot
-plt.scatter(scaled_df[:,0], scaled_df[:,1])
-
-#add axis labels
-plt.xlabel(explained_variance +'% of the variance explained' )
-plt.ylabel('')
-
-#add lables to each point
-for i, txt in enumerate(df.index):
-    plt.annotate(txt, (scaled_df[:,0][i]+.03, scaled_df[:,1][i]))
-
-#display scatterplot
-plt.show()
+# Obsoleted
+# files = {
+#     'Afrikaans': '../data/af_afribooms-ud-train.conllu',
+#     'Danish': '../data/da_ddt-ud-train.conllu',
+#     'German': '../data/de_gsd-ud-train.conllu',
+#     'English': '../data/en_ewt-ud-train.conllu',
+#     'Faroese': '../data/fo_oft-ud-test.conllu',
+#     'Frisian': '../data/fy-frysk-ud-all.conllu',
+#     'Icelandic': '../data/is_modern-ud-train.conllu',
+#     'Gronings': '../data/nds-gronings-ud-all.conllu',
+#     'Dutch': '../data/nl_alpino-ud-train.conllu',
+#     'Norwegian': '../data/no_bokmaal-ud-train.conllu',
+#     'Swedish': '../data/sv_talbanken-ud-train.conllu'
+# }
+#
+# lang = dict()
+#
+# for language, file in files.items():
+#     lang[language] = conllu_to_counter(file)
+# df = calculate_distance_all(lang)
+# dfp = calculate_distance_pairwise(lang)
+#
+#
+# #perform multi-dimensional scaling
+# mds = MDS(random_state=0)
+# scaled_df = mds.fit_transform(df)
+#
+# #calculations for explained variance
+# list1 = [0.49640871, 0.66261855, 0.7659065, 0.83728937, 0.88780632, 0.93446863,
+#  0.95885534, 0.97855612, 0.99303231, 1, 1] #This is from the pca analysis not sure if this is the one to use though
+#
+# def distance(x1,y1,x2,y2):
+#     dist = ((x1-x2)**2 + (y1-y2)**2)**.5
+#     return(dist)
+# list2 = []
+# for i in range(13):
+#     if i == 10:
+#         break
+#     list2.append(distance(scaled_df[i,0], scaled_df[i,1], scaled_df[i+1,0], scaled_df[i+1,1]))
+#
+#
+# def variance(a,b):
+#     var = (a - b)**2
+#     return(var)
+# list3 = []
+# for i in range(11):
+#     if i == 10:
+#         break
+#     list3.append(variance(list1[i],list2[i]))
+#
+# explained_variance = str(sum(list3))
+#
+#
+#
+# #create scatterplot
+# plt.scatter(scaled_df[:,0], scaled_df[:,1])
+#
+# #add axis labels
+# plt.xlabel(explained_variance +'% of the variance explained' )
+# plt.ylabel('')
+#
+# #add lables to each point
+# for i, txt in enumerate(df.index):
+#     plt.annotate(txt, (scaled_df[:,0][i]+.03, scaled_df[:,1][i]))
+#
+# #display scatterplot
+# plt.show()
